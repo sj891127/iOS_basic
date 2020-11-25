4e20f391ed60a155d2fb66f807552c424d9d8b41
shenjie <shenjie@peilian.com>
2020-11-25 18:09:00 +0800

    暗黑模式适配

--- iOSBasic/iOSBasic/Common/BasicViewController.m
+++ iOSBasic/iOSBasic/Common/BasicViewController.m
@@ -20,6 +20,22 @@
     [[UINavigationBar appearance] setShadowImage:[UIImage new]];
     [[UINavigationBar appearance] setTintColor:RGB(60, 64, 76)];
     [[UINavigationBar appearance] setTitleTextAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:18 weight:UIFontWeightMedium],NSForegroundColorAttributeName:RGB(60, 64, 76)}];
+    if (@available(iOS 13.0, *)){
+        self.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
+    }
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self setNeedsStatusBarAppearanceUpdate];
+}
+
+#pragma mark - StatusBar
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    if (@available(iOS 13.0, *)) {
+        return UIStatusBarStyleDarkContent;
+    }
+    return UIStatusBarStyleDefault;
 }
 
 @end
